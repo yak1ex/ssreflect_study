0b822a7cacd6f5b56b8255e25cde6e4838565223
Yak! <yak1ex@users.noreply.github.com>
2022-01-18 22:27:15 +0900

    Add section 6 contents.

--- ssreflect6.v
+++ ssreflect6.v
@@ -72,3 +72,39 @@ Proof.
         exact Hgn.
 Qed.
 (* addKn 使ってない *)
+
+Check odd_mul. (* forall m n : nat, odd (m * n) = odd m && odd n *)
+Check odd_double. (* forall n : nat, odd n.*2 = false *)
+Check odd_double_half. (* forall n: nat, odd n + n./2.*2 = n *)
+Check andbb. (* idempotent andb *)(* forall x: bool, x && x = x *)
+Check negbTE. (* forall b: bool, ~~ b -> b = false *)
+Check double_inj. (* injective double *)(* forall x x2: nat, x.*2 = x2.*2 -> x = x2 *)
+Check divn2. (* forall m: nat, m %/ 2 = m./2 *)
+Check ltn_Pdiv. (* forall m d: nat, 1 < d -> 0 < m -> m %/ d < m *)
+Check muln2. (* forall m: nat, m * 2 = m.*2 *)
+Check esym. (* ?x = ?y -> ?y = ?x *)(* forall (A: Type) (x y: A), x = y -> y = x *)
+
+Lemma odd_square n : odd n = odd (n*n). Admitted.
+Lemma even_double_half n : ~~odd n -> n./2.*2 = n. Admitted.
+
+(* 本定理 *)
+Theorem main_thm (n p : nat) : n * n = (p * p).*2 -> p = 0.
+Proof.
+    elim/lt_wf_ind: n p => n. (* 清楚帰納法 *)
+    case: (posnP n) => [-> _ [] // | Hn IH p Hnp].
+Admitted.
+
+(* 無理数 *)
+Require Import Reals Field. (* 実数とそのための field タクティク *)
+
+Definition irrational (x : R) : Prop :=
+    forall (p q : nat), q <> 0 -> x <> (INR p / INR q)%R.
+
+Theorem irrational_sqrt_2: irrational (sqrt (INR 2)).
+Proof.
+    move=> p q Hq Hrt.
+    apply /Hq /(main_thm p) /INR_eq.
+    rewrite -mul2n !mult_INR -(sqrt_def (INR 2)) ?{}Hrt; last by auto with real.
+    have Hqr : INR q <> 0%R by auto with real.
+    by field.
+Qed.
