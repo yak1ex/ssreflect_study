8501188191c5b094cbda2c9d34e9d93f0795f60c
Yak! <yak1ex@users.noreply.github.com>
2022-02-04 23:40:30 +0900

    Add scripts for section 10 and 11.

---
+++ ssreflect10.v
@@ -0,0 +1,274 @@
+From mathcomp Require Import all_ssreflect all_algebra. (* 代数ライブラリ *)
+
+Local Open Scope ring_scope. (* 環構造を使う *)
+Import GRing.Theory.
+
+Section Problem1.
+Variable K : fieldType. (* 体 *)
+Variable E : vectType K. (* 有限次元ベクトル空間 *)
+Variable f : 'End(E). (* 線形変換 *)
+
+Theorem equiv1 :
+  (limg f + lker f)%VS = fullv <-> limg f = limg (f \o f).
+Proof.
+  split.
+  - move/(f_equal (lfun_img f)).
+    rewrite limg_comp limg_add.
+    admit.
+  - rewrite limg_comp => Hf'.
+    move: (limg_ker_dim f (limg f)).
+    rewrite -[RHS]add0n -Hf' => /eqP.
+    rewrite eqn_add2r dimv_eq0 => /eqP /dimv_disjoint_sum.
+Admitted.
+End Problem1.
+
+Section Problem2.
+Variable K : numFieldType. (* ノルム付き体 *)
+Variable E : vectType K.
+Variable p q : 'End(E).
+
+Definition projection (f : 'End(E)) := forall x, f (f x) = f x.
+
+Lemma proj_idE f : projection f <-> {in limg f, f =1 id}.
+Proof.
+  split => Hf x.
+  - by move/limg_lfunVK => <-.
+  - by rewrite Hf // memv_img ?memvf.
+Qed.
+
+Hypothesis proj_p : projection p.
+Hypothesis proj_q : projection q.
+
+Section a.
+Lemma f_g_0 f g x :
+  projection f -> projection g -> projection (f+g) -> f (g x) = 0.
+Proof.
+  move=> Pf Pg /(_ (g x)).
+  rewrite !add_lfunE !linearD /=.
+  rewrite !Pf !Pg => /eqP.
+  rewrite -subr_eq !addrA addrK.
+  rewrite addrAC eq_sym -subr_eq eq_sym subrr => /eqP Hfg.
+  move: (f_equal g Hfg).
+  rewrite !linearD /= Pg linear0 => /eqP.
+Admitted.
+
+Theorem equiv2 :
+  projection (p + q) <-> (forall x, p (q x) = 0 /\ q (p x) = 0).
+Proof.
+  split=> H x.
+Admitted.
+End a.
+
+Section b.
+Hypothesis proj_pq : projection (p + q).
+
+Lemma b1a x : x \in limg p -> x \in limg q -> x = 0.
+Admitted.
+
+Lemma b1b : directv (limg p + limg q).
+Proof.
+  apply/directv_addP/eqP.
+  rewrite -subv0.
+  apply/subvP => u /memv_capP [Hp Hq].
+  rewrite memv0.
+Admitted.
+
+Lemma limg_sub_lker f g :
+  projection f -> projection g -> projection (f+g) -> (limg f <= lker g)%VS.
+Admitted.
+
+Lemma b1c : (limg p <= lker q)%VS. Admitted.
+
+Lemma b1c' : (limg q <= lker p)%VS. Admitted.
+
+Lemma limg_addv (f g : 'End(E)) : (limg (f + g)%R <= limg f + limg g)%VS.
+Proof.
+  apply/subvP => x /memv_imgP [u _ ->].
+Admitted.
+
+Theorem b1 : limg (p+q) = (limg p + limg q)%VS.
+Proof.
+  apply/eqP; rewrite eqEsubv limg_addv /=.
+  apply/subvP => x /memv_addP [u Hu] [v Hv ->].
+  have -> : u + v = (p + q) (u + v).
+  rewrite lfun_simp !linearD /=.
+  rewrite (proj1 (proj_idE p)) // (proj1 (proj_idE q) _ v) //.
+Admitted.
+Theorem b2 : lker (p+q) = (lker p :&: lker q)%VS.
+Proof.
+  apply/vspaceP => x.
+  rewrite memv_cap !memv_ker.
+  rewrite add_lfunE.
+  case Hpx: (p x == 0).
+Admitted.
+End b.
+End Problem2.
+
+(* ベクトル空間について *)
+Check lkerE.
+(*
+Lemma lkerE f U : (U <= lker f)%VS = (f @: U == 0)%VS.
+  : forall (K : fieldType) (aT rT : vectType K) 
+        (f : 'Hom(aT, rT)) (U : {vspace aT}),
+      (U <= lker f)%VS = ((f @: U)%VS == 0%VS)
+*)
+Check subvv.
+(*
+Lemma subvv U : (U <= U)%VS.
+  : forall (K : fieldType) (vT : vectType K) (U : {vspace vT}),
+      (U <= U)%VS
+*)
+Check subv0.
+(*
+Lemma subv0 U : (U <= 0)%VS = (U == 0%VS).
+  : forall (K : fieldType) (vT : vectType K) (U : {vspace vT}),
+      (U <= 0)%VS = (U == 0%VS)
+*)
+Check addv0.
+(*
+Lemma addv0 : right_id 0%VS addV.
+  : forall (K : fieldType) (vT : vectType K), right_id 0%VS addv
+*)
+Check capfv.
+(*
+Lemma capfv : left_id fullv capV.
+  : forall (K : fieldType) (vT : vectType K), left_id fullv capv
+*)
+Check subvf.
+(*
+Lemma subvf U : (U <= fullv)%VS.
+  : forall (K : fieldType) (vT : vectType K) (U : {vspace vT}),
+      (U <= fullv)%VS
+*)
+Check memvf.
+(*
+Lemma memvf v : v \in fullv.
+  : forall (K : fieldType) (vT : vectType K) (v : vT), v \in fullv
+*)
+Check memvN.
+(*
+Lemma memvN U v : (- v \in U) = (v \in U).
+  : forall (K : fieldType) (vT : vectType K) (U : {vspace vT}) (v : vT),
+    (- v \in U) = (v \in U)
+*)
+Check memv_add.
+(*
+Lemma memv_add u v U V : u \in U -> v \in V -> u + v \in (U + V)%VS.
+  : forall (K : fieldType) (vT : vectType K) (u v : vT)
+    (U V : {vspace vT}), u \in U -> v \in V -> u + v \in (U + V)%VS
+*)
+Check memv_cap.
+(*
+Lemma memv_cap w U V : (w \in U :&: V)%VS = (w \in U) && (w \in V).
+  : forall (K : fieldType) (vT : vectType K) (w : vT) (U V : {vspace vT}),
+    (w \in (U :&: V)%VS) = (w \in U) && (w \in V)
+*)
+Check memv_img.
+(*
+Lemma memv_img f v U : v \in U -> f v \in (f @: U)%VS.
+  : forall (K : fieldType) (aT rT : vectType K) 
+      (f : 'Hom(aT, rT)) (v : aT) (U : {vspace aT}),
+    v \in U -> f v \in (f @: U)%VS
+*)
+Check memv_ker.
+(*
+Lemma memv_ker f v : (v \in lker f) = (f v == 0).
+  : forall (K : fieldType) (aT rT : vectType K) 
+    (f : 'Hom(aT, rT)) (v : aT), (v \in lker f) = (f v == 0)
+*)
+Check limg_ker_dim.
+(*
+Lemma limg_ker_dim f U : (\dim (U :&: lker f) + \dim (f @: U) = \dim U)%N.
+  : forall (K : fieldType) (aT rT : vectType K) 
+      (f : 'Hom(aT, rT)) (U : {vspace aT}),
+    (\dim (U :&: lker f) + \dim (f @: U))%N = \dim U
+*)
+Check dimv_disjoint_sum.
+(*
+Lemma dimv_disjoint_sum U V :
+  (U :&: V = 0)%VS -> \dim (U + V) = (\dim U + \dim V)%N.
+  : forall (K : fieldType) (vT : vectType K) (U V : {vspace vT}),
+  (U :&: V)%VS = 0%VS -> \dim (U + V) = (\dim U + \dim V)%N
+*)
+Check dimv_eq0.
+(*
+Lemma dimv_eq0 U : (\dim U == 0%N) = (U == 0%VS).
+  : forall (K : fieldType) (vT : vectType K) (U : {vspace vT}),
+    (\dim U == 0) = (U == 0%VS)
+*)
+Check eqEdim.
+(*
+Lemma eqEdim U V : (U == V) = (U <= V)%VS && (\dim V <= \dim U).
+  : forall (K : fieldType) (vT : vectType K) (U V : {vspace vT}),
+    (U == V) = (U <= V)%VS && (\dim V <= \dim U)%N
+*)
+Check eqEsubv.
+(*
+Lemma eqEsubv U V : (U == V) = (U <= V <= U)%VS.
+  : forall (K : fieldType) (vT : vectType K) (U V : {vspace vT}),
+    (U == V) = (U <= V <= U)%VS
+*)
+Check vspaceP.
+(*
+Lemma vspaceP U V : U =i V <-> U = V.
+  : forall (K : fieldType) (vT : vectType K) (U V : {vspace vT}),
+    U =i V <-> U = V
+*)
+
+(* 環と体について *)
+Check addr0.
+(*
+Lemma addr0 : right_id 0 +%R.
+  : forall V : zmodType, right_id 0 +%R
+*)
+Check addrA.
+(*
+Lemma addrA : associative +%R.
+  : forall V : zmodType, associative +%R
+*)
+Check addrC.
+(*
+Lemma addrC : commutative +%R.
+  : forall V : zmodType, commutative +%R
+*)
+Check subr_eq.
+(*
+Lemma subr_eq x y z : (x - z == y) = (x == y + z).
+  : forall (V : zmodType) (x y z : V), (x - z == y) = (x == y + z)
+*)
+Check mulr2n.
+(*
+Lemma mulr2n x : x *+ 2 = x + x.
+  : forall (V : zmodType) (x : V), x *+ 2 = x + x
+*)
+Check scaler_nat.
+(*
+Lemma scaler_nat n v : n%:R *: v = v *+ n.
+  : forall (R : ringType) (V : lmodType R) (n : nat) (v : V),
+    n%:R *: v = v *+ n
+*)
+Check scaler_eq0.
+(*
+Lemma scaler_eq0 a v : (a *: v == 0) = (a == 0) || (v == 0).
+  : forall (F : fieldType) (V : lmodType F) (a : F) (v : V),
+    (a *: v == 0) = (a == 0) || (v == 0)
+*)
+Check linear0.
+(*
+Lemma linear0 (f : {linear U -> V | s}) : f 0 = 0.
+  : forall (R : ringType) (U : lmodType R) (V : zmodType)
+      (s : R -> V -> V) (f : {linear U -> V | s}), 
+    f 0 = 0
+*)
+Check linearD.
+(*
+Lemma linearD (f : {linear U -> V | s}) : {morph f : x y / x + y}.
+  : forall (R : ringType) (U : lmodType R) (V : zmodType)
+      (s : R -> V -> V) (f : {linear U -> V | s}),
+    {morph f : x y / x + y >-> x + y}
+*)
+Check Num.Theory.pnatr_eq0.
+(*
+Lemma Num.Theory.pnatr_eq0 n : (n%:R == 0 :> R) = (n == 0)%N.
+  : forall (R : numDomainType) (n : nat), (n%:R == 0) = (n == 0)
+*)
---
+++ ssreflect11.v
@@ -0,0 +1,201 @@
+Require Import ZArith Extraction.
+From mathcomp Require Import all_ssreflect.
+
+(* Simple Calculator *)
+
+Module Simple.
+Inductive expr : Set := (* 変数を含む整数式 *)
+  | Cst of Z
+  | Var of nat
+  | Add of expr & expr
+  | Min of expr
+  | Mul of expr & expr.
+
+Fixpoint eval (env : list Z) (e : expr) : Z := (* 評価関数 *)
+  match e with
+  | Cst x => x
+  | Var n => nth 0 env n (* 変数の値は env で与えられる *)
+  | Add e1 e2 => eval env e1 + eval env e2
+  | Min e1 => 0-eval env e1
+  | Mul e1 e2 => eval env e1 * eval env e2
+  end%Z.
+
+Inductive code : Set := (* 逆ポーランド記法による計算譜 *)
+  | Cimm of Z
+  | Cget of nat
+  | Cadd
+  | Cmin
+  | Cmul.
+
+Fixpoint eval_code (stack : list Z) (l : list code) :=
+  match l with
+  | nil => stack
+  | c :: l' =>
+    let stack' := (* 各コマンドがスタックを変える *)
+        match c, stack with
+        | Cimm x, _ => x :: stack
+        | Cget n, _ => nth 0 stack n :: stack
+        | Cadd, x :: y :: st => x+y :: st
+        | Cmin, x :: st => 0-x :: st
+        | Cmul, x :: y :: st => x*y :: st
+        | _, _ => nil
+        end%Z
+    in eval_code stack' l'
+  end.
+
+Fixpoint compile d (e : expr) : list code :=
+  match e with
+  | Cst x => [:: Cimm x]
+  | Var n => [:: Cget (d+n)]
+  | Add e1 e2 => compile d e2 ++ compile (S d) e1 ++ [:: Cadd]
+  | Min e1 => compile d e1 ++ [:: Cmin]
+  | Mul e1 e2 => compile d e2 ++ compile (S d) e1 ++ [:: Cmul]
+  end.
+
+Lemma eval_code_cat stack l1 l2 :
+  eval_code stack (l1 ++ l2) = eval_code (eval_code stack l1) l2.
+Proof. by elim: l1 stack => //=. Qed.
+
+Theorem compile_correct e d stack : (* コンパイラの正しさ *)
+  eval_code stack (compile d e) = eval (drop d stack) e :: stack.
+Proof.
+  elim: e d stack => //= [n|e1 IHe1 e2 IHe2|e IHe|e1 IHe1 e2 IHe2] d stack.
+  - by rewrite nth_drop.
+  - by rewrite eval_code_cat IHe2 eval_code_cat IHe1.
+Admitted.
+End Simple.
+
+(* Iterating calculator *)
+
+Module Iterator.
+Inductive expr : Set :=
+  | Cst of Z
+  | Var of nat
+  | Add of expr & expr
+  | Min of expr
+  | Mul of expr & expr.
+
+Fixpoint eval (env : list Z) (e : expr) : Z :=
+  match e with
+  | Cst x => x
+  | Var n => nth 0 env n
+  | Add e1 e2 => eval env e1 + eval env e2
+  | Min e1 => 0-eval env e1
+  | Mul e1 e2 => eval env e1 * eval env e2
+  end%Z.
+
+Inductive cmd : Set :=
+  | Assign of nat & expr (* env[n] に結果を入れる *)
+  | Seq of cmd & cmd (* 順番に実行 *)
+  | Repeat of expr & cmd. (* n 回繰り返す *)
+
+Fixpoint eval_cmd (env : list Z) (c : cmd) : list Z :=
+  match c with
+  | Assign n e => set_nth 0%Z env n (eval env e)
+  | Seq c1 c2 => eval_cmd (eval_cmd env c1) c2
+  | Repeat e c =>
+    if eval env e is Zpos n (* seq の iter を使う *)
+    then iter (Pos.to_nat n) (fun e => eval_cmd e c) env
+    else env
+  end.
+
+Inductive code : Set :=
+  | Cimm of Z
+  | Cget of nat
+  | Cadd
+  | Cmin
+  | Cmul
+  | Cset of nat (* スタックの上を n 番目に書き込む *)
+  | Crep of nat (* 次の n 個の命令ををスタックの上分繰り返す *)
+  | Cnext. (* 終ったら Cnext の後ろに跳ぶ *)
+
+Fixpoint eval_code (stack : list Z) (l : list code) :=
+  match l with
+  | nil => stack
+  | c :: l' =>
+    let stack' :=
+        match c, stack with
+        | Cimm x, _ => x :: stack
+        | Cget n, _ => nth 0 stack n :: stack
+        | Cadd, x :: y :: st => x+y :: st
+        | Cmin, x :: st => 0-x :: st
+        | Cmul, x :: y :: st => x*y :: st
+        | Cset n, x :: st => set_nth 0%Z st n x
+        | Crep _, Zpos n :: st =>
+          iter (Pos.to_nat n) (fun st => eval_code st l') st
+        | Crep _, _ :: st => st
+        | Cnext, _ => stack
+        | _, _ => nil
+        end%Z
+    in
+    match c with
+    | Crep n => eval_drop n stack' l' (* Crep の後はコードを飛ばす *)
+    | Cnext => stack' (* Cnext は評価を止める *)
+    | _ => eval_code stack' l' (* 他の場合は続ける *)
+    end
+  end
+with eval_drop n st (l : list code) := (* 相互再帰 *)
+  match l, n with
+  | _ :: l', 0 => eval_code st l'
+  | _ :: l', S n' => eval_drop n' st l'
+  | [::], _ => st
+  end.
+
+Fixpoint compile d (e : expr) : list code :=
+  match e with
+  | Cst x => [:: Cimm x]
+  | Var n => [:: Cget (d+n)]
+  | Add e1 e2 => compile d e2 ++ compile (S d) e1 ++ [:: Cadd]
+  | Min e1 => compile d e1 ++ [:: Cmin]
+  | Mul e1 e2 => compile d e2 ++ compile (S d) e1 ++ [:: Cmul]
+  end.
+
+Fixpoint compile_cmd (c : cmd) : list code :=
+  match c with
+  | Assign n e => compile 0 e ++ [:: Cset n]
+  | Seq c1 c2 => compile_cmd c1 ++ compile_cmd c2
+  | Repeat e c =>
+      let l := compile_cmd c in
+      compile 0 e ++ [:: Crep (length l)] ++ l ++ [:: Cnext]
+  end.
+
+Definition neutral c :=
+  match c with Cnext | Crep _ => false | _ => true end.
+
+Inductive balanced : list code -> Prop := (* Crep と Cnext の対応が取れている *)
+  | Bneutral : forall c, neutral c = true -> balanced [:: c]
+  | Bcat : forall l1 l2, balanced l1 -> balanced l2 -> balanced (l1 ++ l2)
+  | Brep : forall l, balanced l ->
+                     balanced (Crep (length l) :: l ++ [:: Cnext]).
+
+Hint Constructors balanced.
+
+Lemma eval_drop_cat st l1 l2 :
+  eval_drop (length l1) st (l1 ++ Cnext :: l2) = eval_code st l2.
+Admitted.
+
+Check eq_iter. (* 証明に使える *)
+Lemma eval_code_cat stack (l1 l2 : seq code) :
+  balanced l1 ->
+  eval_code stack (l1 ++ l2) =
+  eval_code (eval_code stack l1) l2.
+Admitted.
+
+Lemma compile_balanced n e : balanced (compile n e).
+Proof. by elim: e n => /=; auto. Qed.
+
+Theorem compile_correct e d stack :
+  eval_code stack (compile d e) = eval (drop d stack) e :: stack.
+Admitted.
+
+Lemma compile_cmd_balanced c : balanced (compile_cmd c).
+Proof. by elim: c => /=; auto using compile_balanced. Qed.
+
+Hint Resolve compile_balanced compile_cmd_balanced.
+
+Theorem compile_cmd_correct c stack :
+  eval_code stack (compile_cmd c) = eval_cmd stack c.
+Admitted.
+End Iterator.
+
+Extraction Iterator.eval_code.
