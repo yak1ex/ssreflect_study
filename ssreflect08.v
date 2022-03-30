bd2edf5687d683dd3fa8f189d3cb603e897d239a
Yak! <yak1ex@users.noreply.github.com>
2022-03-31 00:53:08 +0900

    Use 2-space indentation (refs GH-1).

--- ssreflect08.v
+++ ssreflect08.v
@@ -9,68 +9,68 @@ Variable le_trans: forall x y z, le x y -> le y z -> le x z.
 Variable le_total: forall x y, ~~ le x y -> le y x.
 
 Fixpoint insert a l := match l with
-    | nil => (a :: nil)
-    | b :: l' => if le a b then a :: l else b :: insert a l'
-    end.
+  | nil => (a :: nil)
+  | b :: l' => if le a b then a :: l else b :: insert a l'
+  end.
 
 Fixpoint isort l :=
-    if l is a :: l' then insert a (isort l') else nil.
+  if l is a :: l' then insert a (isort l') else nil.
 
 Fixpoint sorted l := (* all を使って bool 上の述語を定義する *)
-    if l is a :: l' then all (le a) l' && sorted l' else true.
+  if l is a :: l' then all (le a) l' && sorted l' else true.
 
 Lemma le_seq_insert a b l :
-    le a b -> all (le a) l -> all (le a) (insert b l).
+  le a b -> all (le a) l -> all (le a) (insert b l).
 Proof.
-    elim: l => /= [-> // | c l IH].
-    move=> leab /andP [leac leal].
-    case: ifPn => lebc /=.
-    - by rewrite leab leac.
-    - by rewrite leac IH.
+  elim: l => /= [-> // | c l IH].
+  move=> leab /andP [leac leal].
+  case: ifPn => lebc /=.
+  - by rewrite leab leac.
+  - by rewrite leac IH.
 Qed.
 
 Lemma le_seq_trans a b l :
-    le a b -> all (le b) l -> all (le a) l.
+  le a b -> all (le b) l -> all (le a) l.
 Proof.
-    move=> leab /allP lebl.
-    apply/allP => x Hx.
-    by apply/le_trans/lebl.
+  move=> leab /allP lebl.
+  apply/allP => x Hx.
+  by apply/le_trans/lebl.
 Qed.
 
 Theorem insert_ok a l : sorted l -> sorted (insert a l).
 Proof.
-    elim: l => //= a0 l0 IH1 /andP[IHL IHR].
-    case: ifPn => Hle /=.
-    - by rewrite Hle (le_seq_trans _ _ _ Hle IHL) IHL IHR.
-    - rewrite le_seq_insert => //.
-        + by rewrite (IH1 IHR).
-        + rewrite le_total => //.
+  elim: l => //= a0 l0 IH1 /andP[IHL IHR].
+  case: ifPn => Hle /=.
+  - by rewrite Hle (le_seq_trans _ _ _ Hle IHL) IHL IHR.
+  - rewrite le_seq_insert => //.
+    + by rewrite (IH1 IHR).
+    + rewrite le_total => //.
 Restart.
-    elim: l => //= a0 l0 IH1 /andP[IHL IHR].
-    case: ifPn => Hle /=.
-    - by rewrite Hle (le_seq_trans _ _ _ Hle IHL) IHL IHR.
-    - by rewrite (le_seq_insert _ _ _ (le_total _ _ Hle) IHL) (IH1 IHR).
+  elim: l => //= a0 l0 IH1 /andP[IHL IHR].
+  case: ifPn => Hle /=.
+  - by rewrite Hle (le_seq_trans _ _ _ Hle IHL) IHL IHR.
+  - by rewrite (le_seq_insert _ _ _ (le_total _ _ Hle) IHL) (IH1 IHR).
 Qed.
 Theorem isort_ok l : sorted (isort l).
 Proof.
-    elim: l => //= a l IH.
-    by apply insert_ok.
+  elim: l => //= a l IH.
+  by apply insert_ok.
 Qed.
 
 (* perm_eq が seq で定義されているが補題だけを使う *)
 Theorem insert_perm l a : perm_eq (a :: l) (insert a l).
 Proof.
-    elim: l => //= b l pal.
-    case: ifPn => //= leab.
-    by rewrite (perm_catCA [:: a] [:: b]) perm_cons.
+  elim: l => //= b l pal.
+  case: ifPn => //= leab.
+  by rewrite (perm_catCA [:: a] [:: b]) perm_cons.
 Qed.
 
 (* perm_trans : forall (T : eqType), transitive (seq T) perm_eq *)
 Theorem isort_perm l : perm_eq l (isort l).
 Proof.
-    elim: l => //= a l IH. (* perm_eq_trans が perm_trans に変わった *)
-    apply/perm_trans/insert_perm.
-    by rewrite perm_cons.
+  elim: l => //= a l IH. (* perm_eq_trans が perm_trans に変わった *)
+  apply/perm_trans/insert_perm.
+  by rewrite perm_cons.
 Qed.
 End sort.
 
@@ -79,29 +79,29 @@ Definition isortn : seq nat -> seq nat := isort _ leq.
 Definition sortedn := sorted _ leq.
 Lemma leq_total a b : ~~ (a <= b) -> b <= a.
 Proof.
-    move: (leq_total a b) => /orP[HL|HR] H //.
-    case: ((negP H) HL).
+  move: (leq_total a b) => /orP[HL|HR] H //.
+  case: ((negP H) HL).
 Restart.
-    Search "~~" "<=".
-    rewrite -ltnNge.
-    Search (_ < _ -> _ <= _).
-    by apply: ltnW.
+  Search "~~" "<=".
+  rewrite -ltnNge.
+  Search (_ < _ -> _ <= _).
+  by apply: ltnW.
 Qed.
 
 Theorem isortn_ok l : sortedn (isortn l) && perm_eq l (isortn l).
 Proof.
-    apply/andP. split.
-    - apply: isort_ok.
-        + Check leq_trans. (* 引数の順番が違う *)
-          move=>m n p. apply: leq_trans.
-        + apply: leq_total.
-    - apply: isort_perm.
+  apply/andP. split.
+  - apply: isort_ok.
+    + Check leq_trans. (* 引数の順番が違う *)
+      move=>m n p. apply: leq_trans.
+    + apply: leq_total.
+  - apply: isort_perm.
 Restart.
-    apply/andP. split.
-    - apply: isort_ok => m n p.
-        + by apply: leq_trans.
-        + by apply: leq_total.
-    - by apply: isort_perm.
+  apply/andP. split.
+  - apply: isort_ok => m n p.
+    + by apply: leq_trans.
+    + by apply: leq_total.
+  - by apply: isort_perm.
 Qed.
 
 Require Import Extraction.
@@ -125,9 +125,9 @@ Proof. elim: n => // n. by rewrite addnS /= negbK. Qed.
 (* 等式を使って n に対する通常の帰納法を可能にする *)
 Theorem even_plus m n : even m -> even n = even (m + n).
 Proof.
-    elim: n => /= [|n IH] Hm.
-    - by rewrite addn0.
-    - by rewrite addnS IH.
+  elim: n => /= [|n IH] Hm.
+  - by rewrite addn0.
+  - by rewrite addnS IH.
 Qed.
 
 Theorem one_not_even : ~~ even 1.
@@ -138,33 +138,33 @@ Proof. done. Qed.
 
 Theorem even_odd n : even n -> odd n.+1.
 Proof.
-    elim: n => //.
+  elim: n => //.
 Restart.
-    done.
+  done.
 Qed.
 Theorem odd_even n : odd n -> even n.+1.
 Proof.
-    elim: n => //= n IH.
-    rewrite negbK => //.
+  elim: n => //= n IH.
+  rewrite negbK => //.
 Restart.
-    elim: n => //= _ _ -> //.
+  elim: n => //= _ _ -> //.
 Qed.
 Theorem even_or_odd n : even n || odd n.
 Proof.
-    by rewrite orbC orbN.
+  by rewrite orbC orbN.
 Restart.
-    rewrite orbC.
-    rewrite orbN.
-    done.
+  rewrite orbC.
+  rewrite orbN.
+  done.
 Qed.
 Theorem odd_odd_even m n : odd m -> odd n = even (m+n).
 Proof.
-    elim: m => //= n0 IH1 IH2.
-    by rewrite -(even_plus n0 _ IH2) negbK.
+  elim: m => //= n0 IH1 IH2.
+  by rewrite -(even_plus n0 _ IH2) negbK.
 Restart.
-    elim: m => //= n0 IH1 IH2.
-    rewrite -(even_plus n0).
-    - rewrite negbK. reflexivity.
-    - exact IH2.
+  elim: m => //= n0 IH1 IH2.
+  rewrite -(even_plus n0).
+  - rewrite negbK. reflexivity.
+  - exact IH2.
 Qed.
 End even_odd.
--- ssreflect09.v
+++ ssreflect09.v
@@ -46,93 +46,93 @@ Proof. by rewrite /Sk big_nat1 exp1n. Qed.
 
 Lemma Tm1 : Tm 1 = 2^m - 2.
 Proof.
-    rewrite /Tm.
-    rewrite [in 2^m](_ : 2 = 1+1) //.
-    rewrite Pascal. (* 二項公式 *)
-    transitivity ((\sum_(0 <= k < m.+1) 'C(m,k)) - 2).
-        symmetry.
-        rewrite (@big_cat_nat _ _ _ m) //=.
-        rewrite (@big_cat_nat _ _ _ 1) //=; last by apply ltnW.
-        rewrite addnAC !big_nat1 bin0 binn addKn.
-        apply eq_bigr => i H.
-        by rewrite Sk1 muln1.
-    rewrite big_mkord.
-    congr (_ - _).
-    apply eq_bigr => i _.
-    by rewrite !exp1n !muln1.
+  rewrite /Tm.
+  rewrite [in 2^m](_ : 2 = 1+1) //.
+  rewrite Pascal. (* 二項公式 *)
+  transitivity ((\sum_(0 <= k < m.+1) 'C(m,k)) - 2).
+    symmetry.
+    rewrite (@big_cat_nat _ _ _ m) //=.
+    rewrite (@big_cat_nat _ _ _ 1) //=; last by apply ltnW.
+    rewrite addnAC !big_nat1 bin0 binn addKn.
+    apply eq_bigr => i H.
+    by rewrite Sk1 muln1.
+  rewrite big_mkord.
+  congr (_ - _).
+  apply eq_bigr => i _.
+  by rewrite !exp1n !muln1.
 Qed.
 
 Search (_ ^ _) "exp". (* 自然数の指数関数 expn に関する様々な補題 *)
 
 Lemma Tm2 : Tm 2 = 3^m - 3.
 Proof.
-    rewrite /Tm.
-    have ->: 3^m - 3 = 2^m - 2 + (3^m - 1 - 2^m).
-        rewrite addnBAC.
-        rewrite subnAC addnBA.
-        rewrite subnKC.
-        by rewrite -subnDA.
-        rewrite leq_exp2r //.
-        by apply ltn_trans with 1.
-        rewrite subn_gt0 ltn_exp2r //.
-        by apply ltn_trans with 1.
-        apply ltn_trans with 2 => //.
-        by rewrite -{1}(expn1 2) ltn_exp2l.
-    rewrite -Tm1.
-    rewrite [in 3^m](_ : 3 = 1+2) //.
-    rewrite Pascal.
-    transitivity (Tm 1 + (\sum_(1 <= k < m) 'C(m,k) * 2^k)).
-        rewrite -big_split /=.
-        apply eq_bigr => i _.
-        rewrite /Sk !big_cons !big_nil.
-        by rewrite !addn0 -mulnDr.
-    congr (_ + _).
-    transitivity ((\sum_(0 <= k < m.+1) 'C(m,k) * 2^k) - 1 - 2^m).
-        symmetry.
-        rewrite (@big_cat_nat _ _ _ 1) //=.
-        rewrite addnC big_nat1 bin0 expn0 mul1n addnK.
-        rewrite (@big_cat_nat _ _ _ m) //=; last by apply ltnW.
-        by rewrite big_nat1 binn mul1n addnK.
-    rewrite big_mkord.
-    do 2 congr (_ - _).
+  rewrite /Tm.
+  have ->: 3^m - 3 = 2^m - 2 + (3^m - 1 - 2^m).
+    rewrite addnBAC.
+    rewrite subnAC addnBA.
+    rewrite subnKC.
+    by rewrite -subnDA.
+    rewrite leq_exp2r //.
+    by apply ltn_trans with 1.
+    rewrite subn_gt0 ltn_exp2r //.
+    by apply ltn_trans with 1.
+    apply ltn_trans with 2 => //.
+    by rewrite -{1}(expn1 2) ltn_exp2l.
+  rewrite -Tm1.
+  rewrite [in 3^m](_ : 3 = 1+2) //.
+  rewrite Pascal.
+  transitivity (Tm 1 + (\sum_(1 <= k < m) 'C(m,k) * 2^k)).
+    rewrite -big_split /=.
     apply eq_bigr => i _.
-    by rewrite exp1n mul1n.
+    rewrite /Sk !big_cons !big_nil.
+    by rewrite !addn0 -mulnDr.
+  congr (_ + _).
+  transitivity ((\sum_(0 <= k < m.+1) 'C(m,k) * 2^k) - 1 - 2^m).
+    symmetry.
+    rewrite (@big_cat_nat _ _ _ 1) //=.
+    rewrite addnC big_nat1 bin0 expn0 mul1n addnK.
+    rewrite (@big_cat_nat _ _ _ m) //=; last by apply ltnW.
+    by rewrite big_nat1 binn mul1n addnK.
+  rewrite big_mkord.
+  do 2 congr (_ - _).
+  apply eq_bigr => i _.
+  by rewrite exp1n mul1n.
 Qed.
 
 Theorem Tmn n : Tm n.+1 = n.+2^m - n.+2.
 Proof.
-    elim:n => [|n IHn] /=.
-        by apply Tm1.
-    have Hm': m > 0 by apply ltnW.
-    have ->: n.+3 ^ m - n.+3 = n.+2 ^ m - n.+2 + (n.+3 ^ m - 1 - n.+2 ^ m).
-        rewrite addnBAC.
-        rewrite subnAC addnBA.
-        rewrite subnKC.
-        by rewrite -subnDA.
-        rewrite leq_exp2r //.
-        rewrite subn_gt0 ltn_exp2r //.
-        by rewrite -{1}(expn1 n.+2) leq_exp2l.
-    rewrite -IHn.
-    rewrite [in n.+3](_ : n.+3 = 1+n.+2) //.
-    rewrite Pascal.
-    transitivity (Tm n.+1 + (\sum_(1 <= k < m) 'C(m,k) * n.+2^k)).
-        rewrite -big_split /=.
-        apply eq_bigr => i _.
-        rewrite /Sk.
-        rewrite (@big_cat_nat _ _ _ n.+2) //=.
-        rewrite big_nat1.
-        by rewrite mulnDr.
-    congr (_ + _).
-    transitivity ((\sum_(0 <= k < m.+1) 'C(m,k) * n.+2^k) - 1 - n.+2^m).
-        symmetry.
-        rewrite (@big_cat_nat _ _ _ 1) //=.
-        rewrite addnC big_nat1 bin0 expn0 mul1n addnK.
-        rewrite (@big_cat_nat _ _ _ m) //=.
-        by rewrite big_nat1 binn mul1n addnK.
-    rewrite big_mkord.
-    do 2 congr (_ - _).
+  elim:n => [|n IHn] /=.
+    by apply Tm1.
+  have Hm': m > 0 by apply ltnW.
+  have ->: n.+3 ^ m - n.+3 = n.+2 ^ m - n.+2 + (n.+3 ^ m - 1 - n.+2 ^ m).
+    rewrite addnBAC.
+    rewrite subnAC addnBA.
+    rewrite subnKC.
+    by rewrite -subnDA.
+    rewrite leq_exp2r //.
+    rewrite subn_gt0 ltn_exp2r //.
+    by rewrite -{1}(expn1 n.+2) leq_exp2l.
+  rewrite -IHn.
+  rewrite [in n.+3](_ : n.+3 = 1+n.+2) //.
+  rewrite Pascal.
+  transitivity (Tm n.+1 + (\sum_(1 <= k < m) 'C(m,k) * n.+2^k)).
+    rewrite -big_split /=.
     apply eq_bigr => i _.
-    by rewrite exp1n mul1n.
+    rewrite /Sk.
+    rewrite (@big_cat_nat _ _ _ n.+2) //=.
+    rewrite big_nat1.
+    by rewrite mulnDr.
+  congr (_ + _).
+  transitivity ((\sum_(0 <= k < m.+1) 'C(m,k) * n.+2^k) - 1 - n.+2^m).
+    symmetry.
+    rewrite (@big_cat_nat _ _ _ 1) //=.
+    rewrite addnC big_nat1 bin0 expn0 mul1n addnK.
+    rewrite (@big_cat_nat _ _ _ m) //=.
+    by rewrite big_nat1 binn mul1n addnK.
+  rewrite big_mkord.
+  do 2 congr (_ - _).
+  apply eq_bigr => i _.
+  by rewrite exp1n mul1n.
 Qed.
 
 Theorem Skp p k : p > 2 -> prime p -> 1 <= k < p.-1 -> p %| Sk k p.-1.
