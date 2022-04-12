9437a7f3d055c7ae7ca43917974c98063130549f
Yak! <yak1ex@users.noreply.github.com>
2022-04-13 01:45:34 +0900

    Add comments.

--- ssreflect09.v
+++ ssreflect09.v
@@ -160,30 +160,30 @@ Proof.
   rewrite [in 3^m](_ : 3 = 1+2) //.
   (* \sum_{k=1}^{m-1} \binom{m}{k} S_k(2) = T_m(1) + ((1+2)^m - 1 - 2^m) *)
   rewrite Pascal.
-  (* \sum_{k=1}^{m-1} \binom{m}{k} S_k(2) = T_m(1) + (\sum_{i<m+1} \binom{m,i}  1^{m-i} 2^i) - 1 - 2^m) *)
+  (* \sum_{k=1}^{m-1} \binom{m}{k} S_k(2) = T_m(1) + (\sum_{i<m+1} \binom{m}{i}  1^{m-i} 2^i) - 1 - 2^m) *)
   transitivity (Tm 1 + (\sum_(1 <= k < m) 'C(m,k) * 2^k)).
-    (* \sum_{k=1}^{m-1} \binom{m}{k} S_k(2) = T_m(1) + (\sum_{k=1}^{m-1} \binom{m,i}  2^k) *)
+    (* \sum_{k=1}^{m-1} \binom{m}{k} S_k(2) = T_m(1) + (\sum_{k=1}^{m-1} \binom{m}{i}  2^k) *)
     rewrite -big_split /=.
-    (* \sum_{k=1}^{m-1} \binom{m}{k} S_k(2) = \sum_{i=1}^{m-1} ( \binom{m,i} S_i(1) + \binom{m,i} 2^i) *)
+    (* \sum_{k=1}^{m-1} \binom{m}{k} S_k(2) = \sum_{i=1}^{m-1} ( \binom{m}{i} S_i(1) + \binom{m}{i} 2^i) *)
     apply eq_bigr => i _. (* 各項等しい *)
-    (* \binom{m}{i} S_i(2) = \binom{m,i} S_i(1) + \binom{m,i} 2^i *)
+    (* \binom{m}{i} S_i(2) = \binom{m}{i} S_i(1) + \binom{m}{i} 2^i *)
     rewrite /Sk !big_cons !big_nil.
     by rewrite !addn0 -mulnDr.
-  (* T_m(1) + (\sum_{k=1}^{m-1} \binom{m,i} 2^k) = T_m(1) + (\sum_{i<m+1} \binom{m,i}  1^{m-i} 2^i) - 1 - 2^m) *)
+  (* T_m(1) + (\sum_{k=1}^{m-1} \binom{m}{i} 2^k) = T_m(1) + (\sum_{i<m+1} \binom{m}{i}  1^{m-i} 2^i) - 1 - 2^m) *)
   congr (_ + _).
-  (* \sum_{k=1}^{m-1} \binom{m,i} 2^k = \sum_{i<m+1} \binom{m,i}  1^{m-i} 2^i - 1 - 2^m *)
+  (* \sum_{k=1}^{m-1} \binom{m}{i} 2^k = \sum_{i<m+1} \binom{m}{i}  1^{m-i} 2^i - 1 - 2^m *)
   transitivity ((\sum_(0 <= k < m.+1) 'C(m,k) * 2^k) - 1 - 2^m).
-    (* \sum_{k=1}^{m-1} \binom{m,i} 2^k = \sum_{k=0}^{m} \binom{m,k} 2^k - 1 - 2^m *)
+    (* \sum_{k=1}^{m-1} \binom{m}{i} 2^k = \sum_{k=0}^{m} \binom{m}{k} 2^k - 1 - 2^m *)
     symmetry.
     rewrite (@big_cat_nat _ _ _ 1) //=.
     rewrite addnC big_nat1 bin0 expn0 mul1n addnK.
     rewrite (@big_cat_nat _ _ _ m) //=; last by apply ltnW.
     by rewrite big_nat1 binn mul1n addnK.
-  (* \sum_{k=0}^{m} \binom{m,k} 2^k - 1 - 2^m = \sum_{i<m+1} \binom{m,i}  1^{m-i} 2^i - 1 - 2^m *)
+  (* \sum_{k=0}^{m} \binom{m}{k} 2^k - 1 - 2^m = \sum_{i<m+1} \binom{m}{i}  1^{m-i} 2^i - 1 - 2^m *)
   rewrite big_mkord.
-  (* \sum_{i<m+1} \binom{m,k} 2^k - 1 - 2^m = \sum_{i<m+1} \binom{m,i}  1^{m-i} 2^i - 1 - 2^m *)
+  (* \sum_{i<m+1} \binom{m}{k} 2^k - 1 - 2^m = \sum_{i<m+1} \binom{m}{i}  1^{m-i} 2^i - 1 - 2^m *)
   do 2 congr (_ - _).
-  (* \sum_{i<m+1} \binom{m,k} 2^k = \sum_{i<m+1} \binom{m,i}  1^{m-i} 2^i *)
+  (* \sum_{i<m+1} \binom{m}{k} 2^k = \sum_{i<m+1} \binom{m}{i}  1^{m-i} 2^i *)
   apply eq_bigr => i _. (* 各項等しい *)
   (* \binom{m,i} 2^i = \binom{m,i}  1^{m-i} 2^i *)
   by rewrite exp1n mul1n.
@@ -193,6 +193,7 @@ Theorem Tmn n : Tm n.+1 = n.+2^m - n.+2.
 Proof.
   elim:n => [|n IHn] /=.
     by apply Tm1.
+  (* T_m(n.+2) = n.+3 ^ m - n.+3 *)
   have Hm': m > 0 by apply ltnW.
   have ->: n.+3 ^ m - n.+3 = n.+2 ^ m - n.+2 + (n.+3 ^ m - 1 - n.+2 ^ m).
     rewrite addnBAC.
@@ -202,26 +203,37 @@ Proof.
     rewrite leq_exp2r //.
     rewrite subn_gt0 ltn_exp2r //.
     by rewrite -{1}(expn1 n.+2) leq_exp2l.
+  (* T_m(n.+2) = n.+2 ^ m - n.+2 + (n.+3 ^ m - 1 - n.+2 ^m) *)
   rewrite -IHn.
+  (* T_m(n.+2) = T_m(n.+1) + (n.+3 ^ m - 1 - n.+2 ^m) *)
   rewrite [in n.+3](_ : n.+3 = 1+n.+2) //.
   rewrite Pascal.
+  (* T_m(n.+2) = T_m(n.+1) + (\sum_{i<m+1} \binom{m}{i} 1^{m-i} n.+2 ^ i) - 1 - n.+2 ^m) *)
   transitivity (Tm n.+1 + (\sum_(1 <= k < m) 'C(m,k) * n.+2^k)).
+    (* T_m(n.+2) = T_m(n.+1) + \sum_{k=1}^{m-1} \binom{m}{k} n.+2^k *)
     rewrite -big_split /=.
     apply eq_bigr => i _.
     rewrite /Sk.
     rewrite (@big_cat_nat _ _ _ n.+2) //=.
     rewrite big_nat1.
     by rewrite mulnDr.
+  (* T_m(n.+1) + \sum_{k=1}^{m-1} \binom{m}{k} n.+2^k = T_m(n.+1) + \sum_{i<m+1} \binom{m}{i} 1^{m-i} n.+2 ^ i - 1 - n.+2 ^m *)
   congr (_ + _).
+  (* \sum_{k=1}^{m-1} \binom{m}{k} n.+2^k = \sum_{i<m+1} \binom{m}{i} 1^{m-i} n.+2 ^ i - 1 - n.+2 ^m *)
   transitivity ((\sum_(0 <= k < m.+1) 'C(m,k) * n.+2^k) - 1 - n.+2^m).
+    (* \sum_{k=1}^{m-1} \binom{m}{k} n.+2^k = \sum_{k=0}^m \binom{m}{k} n.+2^k - 1 - n.+2^m *)
     symmetry.
     rewrite (@big_cat_nat _ _ _ 1) //=.
     rewrite addnC big_nat1 bin0 expn0 mul1n addnK.
     rewrite (@big_cat_nat _ _ _ m) //=.
     by rewrite big_nat1 binn mul1n addnK.
+  (* \sum_{k=0}^m \binom{m}{k} n.+2^k - 1 - n.+2^m = \sum_{i<m+1} \binom{m}{i} 1^{m-i} n.+2 ^ i - 1 - n.+2 ^m *)
   rewrite big_mkord.
+  (* \sum_{i<m+1}^m \binom{m}{i} n.+2^i - 1 - n.+2^m = \sum_{i<m+1} \binom{m}{i} 1^{m-i} n.+2 ^ i - 1 - n.+2 ^m *)
   do 2 congr (_ - _).
+  (* \sum_{i<m+1}^m \binom{m}{i} n.+2^i = \sum_{i<m+1} \binom{m}{i} 1^{m-i} n.+2 ^ i *)
   apply eq_bigr => i _.
+  (* \binom{m}{i} n.+2^i = \binom{m}{i} 1^{m-i} n.+2 ^ i *)
   by rewrite exp1n mul1n.
 Qed.
 
